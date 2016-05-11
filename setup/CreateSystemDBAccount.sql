c80b340b17470036450cd0d81ac404891d23dde5
BUPTSSE-Zero <GGGZ-1101-28@Live.cn>
2016-05-11 23:37:45 +0800

    Fix the bug that Gradle script doesn't work in MS-Windows.

--- setup/CreateSystemDBAccount.sql
+++ setup/CreateSystemDBAccount.sql
@@ -4,6 +4,5 @@
  */
 
 /*Please run this script with root privilege.*/
-DROP USER 'SubwayTicketWeb'@'127.0.0.1';
 CREATE USER 'SubwayTicketWeb'@'127.0.0.1' IDENTIFIED BY '4af8c2909cee0431cae76d45ef740152d144ed51de1af8cde9f836b2158a8bae';
 GRANT ALL ON SubwayTicketDB.* TO 'SubwayTicketWeb'@'127.0.0.1';
